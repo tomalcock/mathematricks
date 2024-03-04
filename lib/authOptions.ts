b15fcd7cf0aa9b5cc3c19c523636e820a780df79
Admin <admin@Admins-MacBook-Air.local>
2024-03-04 12:05:40 +0000

    exported authoptions to different file to try and help redeploy

---
+++ lib/authOptions.ts
@@ -0,0 +1,30 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import prisma from "@/lib/db/prisma";
+import GoogleProvider from "next-auth/providers/google";
+import { Adapter } from "next-auth/adapters";
+import { env } from "@/lib/env";
+import { mergeAnonymousMyCoursesIntoUserCart } from "@/lib/db/mycourses";
+
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+      GoogleProvider({
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+      }),
+    ],
+    callbacks: {
+      session({ session, user }) {
+        session.user.id = user.id;
+        return session;
+      },
+    },
+    events: {
+      async signIn({ user }) {
+        await mergeAnonymousMyCoursesIntoUserCart(user.id);
+      },
+    },
+  };
